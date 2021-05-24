390f1523114e41eefc1c2ab82d54e4a27b2bd909
Tom <tpirescruz@gmail.com>
2021-05-24 18:38:17 +0200

    burger

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const hamburgerToggle = document.querySelector('.hamburger');
+const hamburgerMenu = document.querySelector('.nav-menu')
+
+hamburgerToggle.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('nav-menu-toggle')    
+})
