c2c1e996f75b9b01338322335c1b4a57f8d268b4
Tom <tpirescruz@gmail.com>
2021-05-30 16:39:38 +0200

    js form

--- js/app.js
+++ js/app.js
@@ -1,21 +1,8 @@
 const hamburgerToggle = document.querySelector('.hamburger');
 const hamburgerMenu = document.querySelector('.nav-menu')
-const input = document.querySelectorAll('input')
-const textArea = document.querySelector('textarea')
-const sendButton = document.querySelector('.submit')
+
 
 hamburgerToggle.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('nav-menu-toggle')    
-})
-
-sendButton.addEventListener('click', ()=> {
-    for (const i of input) {
-        if (i.value.length == 0 ) {
-            i.classList.add('required-field')
-        }
-    }
-    if (textArea.value.length == 0) {
-        textArea.classList.add('required-field')
-    }
 })
 
---
+++ js/form.js
@@ -0,0 +1,27 @@
+const input = document.querySelectorAll('input');
+const textArea = document.querySelector('textarea');
+const sendButton = document.querySelector('.submit');
+const inputContainer = document.querySelectorAll('.input-container');
+
+let warning = document.createElement('p');
+
+warning.textContent = 'This field is required';
+warning.classList.add('required');
+
+sendButton.addEventListener('click', () => {
+for (let i of input) {
+    if (i.value.length == 0) {
+        i.parentNode.appendChild(warning)
+        i.classList.add('required-border')
+    } else {
+        i.classList.remove('required-border')
+    };
+}
+
+if (textArea.value.length == 0) {
+    textArea.parentNode.appendChild(warning)
+    textArea.classList.add('required-border')
+} else {
+    textArea.classList.remove('required-border')
+}
+})
